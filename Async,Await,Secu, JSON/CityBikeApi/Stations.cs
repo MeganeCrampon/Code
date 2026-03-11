513018ee620b73fb991b911262befd2770c21508
MeganeCrampon <meganelulu@gmail.com>
2026-03-11 10:48:17 +0100

    class Station simplifiée

--- Async,Await,Secu, JSON/CityBikeApi/Stations.cs
+++ Async,Await,Secu, JSON/CityBikeApi/Stations.cs
@@ -1,12 +1,8 @@
 namespace CityBikeApi;
 public class Station {
-    // L'id de la borne
     public int Id { get; set; }
-    // Le nom de la rue
     public string Nom { get; set; } = string.Empty;
-    // Le nombre de vélos restants
     public int VelosDisponibles { get; set; }
-    // Est-ce que la station fonctionne ?
     public bool EstEnService { get; set; }
     public int CapaciteMax { get; set; }
 }
