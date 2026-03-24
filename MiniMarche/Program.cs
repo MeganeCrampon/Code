b37bf74427466b3ebb6c127e6538bcaae86ec9ae
MeganeCrampon <meganelulu@gmail.com>
2026-03-24 16:53:58 +0100

    Ajout interactivité dans la console

--- MiniMarche/Program.cs
+++ MiniMarche/Program.cs
@@ -1,36 +1,60 @@
-public class Produit
-{
-    public int Id { get; set; }
-    public string Nom { get; set;} = string.Empty;
-    public double Prix { get; set;}
-}
+using System.ComponentModel.Design.Serialization;
 
-class Program
+var catalogue = new List<Produit>
 {
-    static void Main(string[] args) 
+    new Produit { Id = 1, Nom = "Pomme", Prix = 0.80, QuantitéDispo = 15 },
+    new Produit { Id = 2, Nom = "Banane", Prix = 1.20, QuantitéDispo = 22 },
+    new Produit { Id = 3, Nom = "Poire", Prix = 1.10, QuantitéDispo = 13 }
+};
+
+var panier = new List<Produit>();
+
+while (true)
+{
+    Console.WriteLine("\n--- Catalogue disponible ---");
+    foreach (var p in catalogue)
     {
-        var catalogue = new List<Produit>
+        Console.WriteLine($"{p.Id}: {p.Nom} ({p.Prix}€) - Stock : {p.QuantiteDispo}");
+    }
+
+    string input = Console.ReadLine("Quel est l'ID du produit que vous souhaitez ajouter au panier ?");
+
+    if (input == "0") break;
+
+    else if (int.TryParse(input, out int idChoisi))
+    {
+        // [cite_start] ?
+        var produitTrouve = catalogue.Find(p => p.Id == idChoisi);
+
+       /* 
+        if (produitTrouve != null)
         {
-            new Produit { Id = 1, Nom = "Pomme", Prix = 0.80 },
-            new Produit { Id = 2, Nom = "Banane", Prix = 1.20 },
-            new Produit { Id = 3, Nom = "Poire", Prix = 1.10 }
-        };
+            panier.Add(produitTrouve);
+            Console.WriteLine($"{produitTrouve} a été ajouté au panier.");
+        } else
+        {
+            Console.WriteLine("Désolé, l'ID entré est invalide.");
+        }
+        */
 
-        var panier = new List<Produit>();
-            panier.Add(catalogue[0]);
-            panier.Add(catalogue[1]);
-            panier.Add(catalogue[2]);
+        if(produitTrouve == null) 
+            Console.WriteLine($"Désolé, l'ID {produitTrouve.Id} n'a pas été trouvé");
+            return;  
 
-        double prixTotal = 0;
-            foreach (var p in panier)
-            {
-                prixTotal += p.Prix;
-            }
-            if (prixTotal > 20)
-            {
-                prixTotal = prixTotal * 0.90; // remise de 10%
-            }
-        Console.WriteLine("Total : " + prixTotal);
+        panier.Add(produitTrouve);
+        Console.WriteLine($"{produitTrouve} a été ajouté au panier.");  
     }
 }
+
+int prixTotal = 0;
+    foreach (var p in panier)
+    {
+        prixTotal += p.Prix;
+    }
+    if (prixTotal > 20)
+    {
+        prixTotal = prixTotal * 0.90; // remise de 10%
+    }
+Console.WriteLine("Total : " + prixTotal);
+
 
